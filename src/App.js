a6708d30b189118a644fcc20e595830f53c0f90a
jsl202 <jsl202@naver.com>
2023-12-05 11:39:21 +0900

    LifeCycleMethod

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import { Component } from 'react';
-import IterationSample from './06_RepeatComponent/IterationSample';
+import LifeCycleSample from './07_LifeCycleMethod/LifeCycleSample';
+import ErrorBoundary from './07_LifeCycleMethod/ErrorBoundary';
 
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+        <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
